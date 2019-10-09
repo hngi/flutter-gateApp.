6944bf86fbc921d8d222852032af341dcb34595c
Taiwodave <akindele_taiwo57@yahoo.com>
2019-10-09 19:52:10 +0100

    validate added

--- lib/pages/Select_Estate.dart
+++ lib/pages/Select_Estate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gateapp/core/models/user.dart';
 import 'package:gateapp/pages/Add_Estate.dart';
 import 'package:gateapp/utils/colors.dart';
 import 'package:gateapp/utils/helpers.dart';
@@ -12,6 +13,9 @@ class ManageAddress extends StatefulWidget {
 }
 
 class _ManageAddressState extends State<ManageAddress> {
+  final _formkey = GlobalKey<FormState>();
+  Model model = Model();
+
   List<String> _countries = ['Nigeria', 'South Africa', 'China'];
   List<String> _cities = ['Lagos', 'Abuja', 'Imo'];
   List<String> _estates = ['CBS Esate', 'Lux Eco', '1000 Units'];
@@ -33,109 +37,112 @@ class _ManageAddressState extends State<ManageAddress> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(children: <Widget>[
-        ListView(
-          padding: EdgeInsets.symmetric(horizontal: 28.0, vertical: 20.0),
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 50.0),
-              child: Text('Select Your Estate',
-                  style: TextStyle(
-                      fontSize: 32.0,
-                      color: Colors.green,
-                      fontWeight: FontWeight.w600)),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 50.0),
-              child:
-                  Text('Input your current location and estate to set you up',
-                      style: TextStyle(
-                        fontSize: 13.0,
-                        color: Colors.grey,
-                      )),
-            ),
+    return Form(
+      key: _formkey,
+          child: Scaffold(
+        body: Stack(children: <Widget>[
+          ListView(
+            padding: EdgeInsets.symmetric(horizontal: 28.0, vertical: 20.0),
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: Text('Select Your Estate',
+                    style: TextStyle(
+                        fontSize: 32.0,
+                        color: Colors.green,
+                        fontWeight: FontWeight.w600)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 50.0),
+                child:
+                    Text('Input your current location and estate to set you up',
+                        style: TextStyle(
+                          fontSize: 13.0,
+                          color: Colors.grey,
+                        )),
+              ),
 
-            //Select Country
-            CustomDropdownButton(
-              label: 'Select Country',
-              hintText: _countries.first,
-              value: country ?? _countries.first,
-              onChanged: _onCountriesChanged,
-              items: _countries.map((String country) {
-                return DropdownMenuItem(
-                  child: Text(country),
-                  value: country,
-                );
-              }).toList(),
-            ),
+              //Select Country
+              CustomDropdownButton(
+                label: 'Select Country',
+                hintText: _countries.first,
+                value: country ?? _countries.first,
+                onChanged: _onCountriesChanged,
+                items: _countries.map((String country) {
+                  return DropdownMenuItem(
+                    child: Text(country),
+                    value: country,
+                  );
+                }).toList(),
+              ),
 
-            //Select City
-            CustomDropdownButton(
-              label: 'Select City',
-              hintText: _cities.first,
-              value: city ?? _cities.first,
-              onChanged: _onCitiesChanged,
-              items: _cities.map((String city) {
-                return DropdownMenuItem(
-                  child: Text(city),
-                  value: city,
-                );
-              }).toList(),
-            ),
+              //Select City
+              CustomDropdownButton(
+                label: 'Select City',
+                hintText: _cities.first,
+                value: city ?? _cities.first,
+                onChanged: _onCitiesChanged,
+                items: _cities.map((String city) {
+                  return DropdownMenuItem(
+                    child: Text(city),
+                    value: city,
+                  );
+                }).toList(),
+              ),
 
-            //Select Estate
-            CustomDropdownButton(
-              
-              label: 'Select your Estate',
-              prefixIcon: Icon(Icons.search),
-              hintText: _estates.first,
-              value: estate ?? _estates.first,
-              onChanged: _onEstatesChanged,
-              items: _estates.map((String estate) {
-                return DropdownMenuItem(
-                  child: Text(estate),
-                  value: estate,
-                );
-              }).toList(),
-            ),
+              //Select Estate
+              CustomDropdownButton(
+                
+                label: 'Select your Estate',
+                prefixIcon: Icon(Icons.search),
+                hintText: _estates.first,
+                value: estate ?? _estates.first,
+                onChanged: _onEstatesChanged,
+                items: _estates.map((String estate) {
+                  return DropdownMenuItem(
+                    child: Text(estate),
+                    value: estate,
+                  );
+                }).toList(),
+              ),
 
-            //Enter Flat Number
+              //Enter Flat Number
 
-            SizedBox(height: 90.0),
+              SizedBox(height: 90.0),
 
-            //Save Button
-            ActionButton(
-              buttonText: 'Continue',
-              onPressed: () {},
-            )
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 430.0, left: 110),
-          child: Text('Could not find my Estate?',
-              style: TextStyle(
-                  fontSize: 13.0,
-                  color: GateManColors.textColor,
-                  fontWeight: FontWeight.w600)),
-        ),
-        InkWell(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 430.0, left: 270),
-            child: Text('Add New',
+              //Save Button
+              ActionButton(
+                buttonText: 'Continue',
+                onPressed: () {},
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 430.0, left: 110),
+            child: Text('Could not find my Estate?',
                 style: TextStyle(
                     fontSize: 13.0,
-                    color: Colors.green,
-                    fontWeight: FontWeight.w700)),
+                    color: GateManColors.textColor,
+                    fontWeight: FontWeight.w600)),
           ),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => AddEstate()),
-            );
-          },
-        ),
-      ]),
+          InkWell(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 430.0, left: 270),
+              child: Text('Add New',
+                  style: TextStyle(
+                      fontSize: 13.0,
+                      color: Colors.green,
+                      fontWeight: FontWeight.w700)),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddEstate()),
+              );
+            },
+          ),
+        ]),
+      ),
     );
   }
 }
