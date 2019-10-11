945790f768f187046affea58744601d45ff18a68
Remi-cm <remidongmophp@gmail.com>
2019-10-11 15:05:29 +0100

    Route to gateman
    This is the route to the gateman registration page. This page is the entry point of my part. All others pages were linked already manually, You guys won't have to handle their interactions, you just have to add link the QR codes pages to it. Thanks for all. Shalom.

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -27,6 +27,7 @@ import 'package:gateapp/pages/welcome_resident.dart';
 import 'package:gateapp/pages/welcomepage1.dart';
 import 'package:gateapp/pages/add_permission.dart';
 import 'package:gateapp/pages/user_type.dart';
+import 'package:gateapp/pages/gateman/register.dart';
 
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -117,6 +118,9 @@ class Routes {
 
       case '/welcome-resident':
         return MaterialPageRoute(builder: (context) => WelcomeResident());
+        
+      case '/gateman-register':
+        return MaterialPageRoute(builder: (context) => GatemanRegister());
 
 
       default:
