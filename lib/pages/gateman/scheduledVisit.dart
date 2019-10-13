fcb715a90edb6ce80bd3dcb70715d5500fc2b1ca
Remi-cm <remidongmophp@gmail.com>
2019-10-13 18:16:22 +0100

    Scheduled visits
    update

--- lib/pages/gateman/scheduledVisit.dart
+++ lib/pages/gateman/scheduledVisit.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:gateapp/pages/gateman/notifications.dart';
+import 'package:gateapp/pages/gateman/widgets/bottomAppbar.dart';
+import 'package:gateapp/pages/gateman/widgets/customFab.dart';
 import 'package:gateapp/utils/colors.dart';
 import 'package:gateapp/pages/gateman/residents.dart';
+import 'package:gateapp/widgets/GateManBottomNavBar/custom_bottom_nav_bar.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 import 'menu.dart';
 
 class ScheduledVisit extends StatefulWidget {
-  final name;
-  ScheduledVisit({this.name});
+  
   @override
   _ScheduledVisitState createState() => _ScheduledVisitState();
 }
@@ -22,63 +24,8 @@ class _ScheduledVisitState extends State<ScheduledVisit> {
     final hv = MediaQuery.of(context).size.width/100;
     return Scaffold(
       appBar: AppBar(title: Text('Scheduled Visit'), actions: <Widget>[IconButton(icon: Icon(MdiIcons.dotsVertical),onPressed: (){},)],),
-     bottomNavigationBar: new BottomAppBar(
-        color: GateManColors.primaryColor,
-        child: Padding(
-          padding: const EdgeInsets.only(top:15.0, left: 20.0, right: 20.0, bottom: 10.0),
-          child: SizedBox(height: hv*14,
-            child: new Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              InkWell(onTap: (){Navigator.of(context).push(MaterialPageRoute(builder: (context) => new Menu(name: widget.name,)));},
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/images/gateman/menu.png'),
-                    Text('Menu', style: TextStyle(color: Colors.white, fontSize: 13.0, fontWeight: FontWeight.bold ),)
-                  ],
-                ),
-              ),
-              InkWell(onTap: (){Navigator.of(context).push(MaterialPageRoute(builder: (context) => new GatemanNotifications(name: widget.name,)));},
-                child: Column(
-                  children: <Widget>[
-                    Stack(
-                      children: <Widget>[
-                        Image.asset('assets/images/gateman/notification.png'),
-                        badge ? Positioned(
-                          right: 0,
-                          child: new Container(
-                            padding: EdgeInsets.all(1),
-                            decoration: new BoxDecoration(color: Colors.red,borderRadius: BorderRadius.circular(6),),
-                            constraints: BoxConstraints(minWidth: 12,minHeight: 12,),
-                            child: new Text('$_counter',style: new TextStyle(color: Colors.white,fontSize: 8,),textAlign: TextAlign.center,),
-                          ),
-                          ):Container()
-                      ],
-                    ),
-                    Text('Alert', style: TextStyle(color: Colors.white, fontSize: 13.0, fontWeight: FontWeight.bold ))
-                  ],
-                ),
-              ),
-            ],
-            ),
-          ),
-        ),
-      ),
-      floatingActionButton: SizedBox(width: 110.0, height:110.0,
-      child:   new FloatingActionButton(backgroundColor: GateManColors.primaryColor,
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 4.0),
-                child: Image.asset('assets/images/gateman/residents.png'),
-              ),
-              Text('Residents', style: TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold),)
-              ],
-  
-              ), onPressed: () {Navigator.of(context).push(MaterialPageRoute(builder: (context) => new Residents(name: widget.name,)));},
-  
-              ),
-      ),
+     bottomNavigationBar: CustomBottomAppBar(),
+      floatingActionButton: CustomFAB(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: ListView(
         children: <Widget>[
