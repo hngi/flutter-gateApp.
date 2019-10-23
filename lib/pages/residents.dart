3cd6b87a3e54acc01c56cd63f340cab9103152ef
Jude Jonathan <jonathanjude27@gmail.com>
2019-10-23 16:16:02 +0100

    resident

--- lib/pages/residents.dart
+++ lib/pages/residents.dart
@@ -4,6 +4,7 @@ import 'package:gateapp/core/models/user.dart';
 import 'package:gateapp/core/service/gateman_service.dart';
 import 'package:gateapp/pages/gateman_menu.dart';
 import 'package:gateapp/providers/gateman_user_provider.dart';
+import 'package:gateapp/utils/Loader/loader.dart';
 import 'package:gateapp/utils/LoadingDialog/loading_dialog.dart';
 import 'package:gateapp/utils/colors.dart';
 import 'package:gateapp/utils/constants.dart';
@@ -61,81 +62,85 @@ class _ResidentsState extends State<Residents> {
         Provider.of<GatemanUserProvider>(context, listen: false);
 
     return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 28.0, vertical: 20.0),
-        children: <Widget>[
-          SizedBox(height: size.height * 0.06),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 3.0),
-            child: Text('Welcome ${gateManProvider.gatemanUser.fullName}',
-                style: TextStyle(
-                  color: GateManColors.blackColor,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w700,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 7.0),
-            child: Text('Peace Estate',
-                style: TextStyle(
-                  color: GateManColors.primaryColor,
-                  fontSize: 17.0,
-                  fontWeight: FontWeight.w700,
-                )),
-          ),
-          CustomTextFormField(
-            labelName: '',
-            hintText: 'Search by Name, Phone, Address, Visitor Info',
-            onSaved: (str) {},
-            validator: (str) => null,
-            prefixIcon: Icon(Icons.search),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            child: Text('Residents',
-                style: TextStyle(
-                  color: GateManColors.primaryColor,
-                  fontSize: 17.0,
-                  fontWeight: FontWeight.w700,
-                )),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: _residents.map((GatemanResidentVisitors visitor) {
-              User resident = visitor.user;
+      body: isLoading
+          ? Loader.show()
+          : ListView(
+              padding: EdgeInsets.symmetric(horizontal: 28.0, vertical: 20.0),
+              children: <Widget>[
+                SizedBox(height: size.height * 0.06),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 3.0),
+                  child: Text('Welcome ${gateManProvider.gatemanUser.fullName}',
+                      style: TextStyle(
+                        color: GateManColors.blackColor,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w700,
+                      )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 7.0),
+                  child: Text('Peace Estate',
+                      style: TextStyle(
+                        color: GateManColors.primaryColor,
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w700,
+                      )),
+                ),
+                CustomTextFormField(
+                  labelName: '',
+                  hintText: 'Search by Name, Phone, Address, Visitor Info',
+                  onSaved: (str) {},
+                  validator: (str) => null,
+                  prefixIcon: Icon(Icons.search),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text('Residents',
+                      style: TextStyle(
+                        color: GateManColors.primaryColor,
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w700,
+                      )),
+                ),
+                _residents == null || _residents.length == 0
+                    ? Center(
+                        child: Column(
+                        children: <Widget>[
+                          Icon(Icons.hourglass_empty, size: 50.0),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('No Residents'),
+                          )
+                        ],
+                      ))
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children:
+                            _residents.map((GatemanResidentVisitors visitor) {
+                          User resident = visitor.user;
 
-              return ResidentExpansionTile(
-                fullName: '${resident.firstName}  ${resident.lastName}',
-                address: 'Block 3A, Dele Adebayo Estate',
-                phoneNumber: resident.phone,
-                visitText: 'Scheduled Visit',
-                numberCount: '1',
-                visitorApprovalStatus:
-                    visitor.status == 0 ? 'Not Approved' : 'Approved',
-                visitorDescription: visitor.description,
-                visitorETA: '00:00 - 00:00',
-                visitorName: 'John Doe',
-                visitorPhoneNumber: '0812345678',
-                visitorVerifyNo: 'LA7739JA',
-              );
-            }).toList(),
-          ),
-          ResidentExpansionTile(
-            fullName: 'Mr. Seun Adeniyi',
-            address: 'Block 3A, Dele Adebayo Estate',
-            phoneNumber: '0812345678',
-            visitText: 'Scheduled Visit',
-            numberCount: '2',
-            visitorApprovalStatus: 'Approved',
-            visitorDescription: 'Bald, Tall and ..',
-            visitorETA: '00:00 - 00:00',
-            visitorName: 'John Doe',
-            visitorPhoneNumber: '0812345678',
-            visitorVerifyNo: 'LA7739JA',
-          ),
-        ],
-      ),
+                          return ResidentExpansionTile(
+                            fullName:
+                                '${resident.firstName}  ${resident.lastName}',
+                            address: 'Block 3A, Dele Adebayo Estate',
+                            phoneNumber: resident.phone,
+                            visitText: 'Scheduled Visit',
+                            numberCount: '1',
+                            visitorApprovalStatus: visitor.status == 0
+                                ? 'Not Approved'
+                                : 'Approved',
+                            visitorDescription: visitor.description,
+                            visitorETA:
+                                '${visitor.timeIn} - ${visitor.timeOut}',
+                            visitorName: visitor.name,
+                            visitorPhoneNumber: visitor.phoneNo,
+                            visitorVerifyNo: visitor.qrCode,
+                          );
+                        }).toList(),
+                      ),
+              ],
+            ),
       floatingActionButton: BottomNavFAB(
         onPressed: () {
           Navigator.pushReplacement(
