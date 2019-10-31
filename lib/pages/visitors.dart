a209f82b2584f9ee570a417ee63b12c45459d4d7
Remi-cm <remidongmophp@gmail.com>
2019-10-31 13:36:35 +0100

    My Visitors page dynamic UI

--- lib/pages/visitors.dart
+++ lib/pages/visitors.dart
@@ -7,44 +7,54 @@ import 'package:xgateapp/widgets/GateManBottomNavBar/custom_bottom_nav_bar.dart'
 import 'package:xgateapp/widgets/GateManBottomNavFAB/bottom_nav_fab.dart';
 import 'package:xgateapp/widgets/VisitorsTiles/visitorsTile.dart';
 
-
 class MyVisitors extends StatefulWidget {
   @override
   _MyVisitorsState createState() => _MyVisitorsState();
 }
 
 List _visitors = [
-    {
-      "name":"James Cameron",
-      "relation":"Friend",
-      "phone":"0812567898",
-      "date":"2nd Nov 2019",
-      "avatarLink":"assets/images/avatar2.jpg"
-    },
-    {
-      "name":"Mr. Seun Adeyini",
-      "relation":"Father",
-      "phone":"0486332555",
-      "date":"1st Dec 2019",
-      "avatarLink":"assets/images/avatar2.jpg"
-    },
-    {
-      "name":"Mrs. Angelina Taffo",
-      "relation":"Wife",
-      "phone":"6987533255",
-      "date":"3th august 2020",
-      "avatarLink":"assets/images/avatar2.jpg"
-    },
-  ];
+  {
+    "name": "James Cameron",
+    "relation": "Friend",
+    "phone": "0812567898",
+    "date": "2nd Nov 2019",
+    "avatarLink": "assets/images/avatar2.jpg"
+  },
+  {
+    "name": "Mr. Seun Adeyini",
+    "relation": "Father",
+    "phone": "0486332555",
+    "date": "1st Dec 2019",
+    "avatarLink": "assets/images/avatar2.jpg"
+  },
+  {
+    "name": "Mrs. Angelina Taffo",
+    "relation": "Wife",
+    "phone": "6987533255",
+    "date": "3th august 2020",
+    "avatarLink": "assets/images/avatar2.jpg"
+  },
+];
 
 class _MyVisitorsState extends State<MyVisitors> {
-
-    final Map<int, Widget> children = <int, Widget>{
-    0: Padding(child:Text('Scheduled', style: TextStyle(color: scheduledColor),), padding: EdgeInsets.all(10.0),),
-    1: Padding(child:Text('Saved', style: TextStyle(color: savedColor)), padding: EdgeInsets.all(10.0),),
-    2: Padding(child:Text('History', style: TextStyle(color: historyColor)), padding: EdgeInsets.all(10.0),),
+  final Map<int, Widget> children = <int, Widget>{
+    0: Padding(
+      child: Text(
+        'Scheduled',
+        style: TextStyle(color: scheduledColor),
+      ),
+      padding: EdgeInsets.all(10.0),
+    ),
+    1: Padding(
+      child: Text('Saved', style: TextStyle(color: savedColor)),
+      padding: EdgeInsets.all(10.0),
+    ),
+    2: Padding(
+      child: Text('History', style: TextStyle(color: historyColor)),
+      padding: EdgeInsets.all(10.0),
+    ),
   };
-  
+
   static Color scheduledColor = Colors.grey;
   static Color savedColor = Colors.grey;
   static Color historyColor = Colors.grey;
@@ -62,48 +72,48 @@ class _MyVisitorsState extends State<MyVisitors> {
   };
 
   int sharedValue = 0;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: GateManHelpers.appBar(context, 'Visitors'),
       floatingActionButton: BottomNavFAB(
-                   onPressed: () {
-                     Navigator.pushNamed(context, '/add_visitor');
-                   },
-                   icon: MdiIcons.account,
-                   title: 'Visitors',
-                 ),
-                 floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-                 bottomNavigationBar: CustomBottomNavBar(
-                   leadingIcon: MdiIcons.apps,
-                   leadingText: 'Menu',
-                   traillingIcon: MdiIcons.bell,
-                   traillingText: 'Alerts',
-                   onLeadingClicked: () {
-                     print("leading clicked");
-                     Navigator.pushNamed(context, '/homepage');
-                   },
-                   onTrailingClicked: () {
-                     Navigator.pushNamed(context, '/resident-notifications');
-                   },
-                 ),
-        body: Padding(
-          padding: const EdgeInsets.only(top:15.0),
-          child: CupertinoPageScaffold(
-      child: DefaultTextStyle(
-          style: CupertinoTheme.of(context).textTheme.textStyle,
-          child: SafeArea(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  width: 500.0,
-                  child: CupertinoSegmentedControl<int>(
-                    children: children,
-                    onValueChanged: (int newValue) {
-                      setState(() {
-                        sharedValue = newValue;
-                        /*if(newValue == 0){
+        onPressed: () {
+          Navigator.pushNamed(context, '/add_visitor');
+        },
+        icon: MdiIcons.account,
+        title: 'Visitors',
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: CustomBottomNavBar(
+        leadingIcon: MdiIcons.apps,
+        leadingText: 'Menu',
+        traillingIcon: MdiIcons.bell,
+        traillingText: 'Alerts',
+        onLeadingClicked: () {
+          print("leading clicked");
+          Navigator.pushNamed(context, '/homepage');
+        },
+        onTrailingClicked: () {
+          Navigator.pushNamed(context, '/resident-notifications');
+        },
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15.0),
+        child: CupertinoPageScaffold(
+          child: DefaultTextStyle(
+            style: CupertinoTheme.of(context).textTheme.textStyle,
+            child: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    width: 500.0,
+                    child: CupertinoSegmentedControl<int>(
+                      children: children,
+                      onValueChanged: (int newValue) {
+                        setState(() {
+                          sharedValue = newValue;
+                          /*if(newValue == 0){
                         setState(() {
                          scheduledColor = Colors.green; 
                          savedColor = Colors.grey; 
@@ -122,122 +132,157 @@ class _MyVisitorsState extends State<MyVisitors> {
                          historyColor = Colors.green; 
                         });
                       }*/
-                      });
-                      
-                      print('$newValue');
-                      
-                    },
-                    groupValue: sharedValue,
-                    borderColor: Colors.grey.withOpacity(0.2),
-                    selectedColor: Colors.grey.withOpacity(0.2),
-                    padding: const EdgeInsets.all(10.0),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                       
-                      horizontal: 12.0,
-                    ),
-                    child: Container(
-                      child: icons[sharedValue],
+                        });
+
+                        print('$newValue');
+                      },
+                      groupValue: sharedValue,
+                      borderColor: Colors.grey.withOpacity(0.2),
+                      selectedColor: Colors.grey.withOpacity(0.2),
+                      padding: const EdgeInsets.all(10.0),
                     ),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                      ),
+                      child: Container(
+                        child: icons[sharedValue],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-      ),
-    ),
         ),
+      ),
     );
   }
 }
 
 Widget scheduledTab = ListView.builder(
-                      
-        itemCount: _visitors.length,
-        itemBuilder: (BuildContext context, int index){
-          return Padding(
-                  padding: const EdgeInsets.only(bottom:8.0),
-                  child: VisitorTile(
-                      name: _visitors[index]['name'],
-                      phone: _visitors[index]['phone'],
-                      relation: _visitors[index]['relation'],
-                      date: _visitors[index]['date'],
-                      timeline: "Expected Arrival",
-                      buttonText1: "Edit",
-                      buttonText2: "Remove",
-                      buttonFunc1: (){},
-                      avatarLink: _visitors[index]['avatarLink'],
-                      buttonFunc2: (){
-                        showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Are you sure you want to remove visitor ?'),
-            content: Text('Visitor will no longer be in your scheduled liste.'),
-            actions: <Widget>[
-              FlatButton(
-                  onPressed: () {Navigator.pop(context);},
-                  child: Text('Cancel')),
-              FlatButton(
-                onPressed: () {},
-                child: Text('Remove'),
-              )
-            ],
-          );
-        });
-                      },
-                      ),
-          );
+  itemCount: _visitors.length,
+  itemBuilder: (BuildContext context, int index) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: VisitorTile(
+        name: _visitors[index]['name'],
+        phone: _visitors[index]['phone'],
+        relation: _visitors[index]['relation'],
+        date: _visitors[index]['date'],
+        timeline: "Expected Arrival",
+        buttonText1: "Edit",
+        buttonText2: "Remove",
+        buttonFunc1: () {},
+        avatarLink: _visitors[index]['avatarLink'],
+        buttonFunc2: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text('Are you sure you want to remove visitor ?'),
+                  content: Text(
+                      'Visitor will no longer be in your scheduled liste.'),
+                  actions: <Widget>[
+                    FlatButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Cancel')),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text('Remove'),
+                    )
+                  ],
+                );
+              });
         },
-
-      );
+      ),
+    );
+  },
+);
 
 Widget savedTab = ListView.builder(
-                      
-        itemCount: _visitors.length,
-        itemBuilder: (BuildContext context, int index){
-          return Padding(
-                  padding: const EdgeInsets.only(bottom:8.0),
-                  child: VisitorTile(
-                      name: _visitors[index]['name'],
-                      phone: _visitors[index]['phone'],
-                      relation: _visitors[index]['relation'],
-                      date: _visitors[index]['date'],
-                      timeline: "Last Visit",
-                      buttonText1: "Remove",
-                      buttonText2: "Schedule Visit",
-                      buttonFunc1: (){},
-                      buttonFunc2: (){
-                        showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Are you sure you want to remove visitor ?'),
-            content: Text('Visitor will no longer be in your scheduled liste.'),
-            actions: <Widget>[
-              FlatButton(
-                  onPressed: () {Navigator.pop(context);},
-                  child: Text('Cancel')),
-              FlatButton(
-                onPressed: () {},
-                child: Text('Remove'),
-              )
-            ],
-          );
-        });
-                      },
-                      avatarLink: _visitors[index]['avatarLink'],
-                      ),
-          );
+  itemCount: _visitors.length,
+  itemBuilder: (BuildContext context, int index) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: VisitorTile(
+        name: _visitors[index]['name'],
+        phone: _visitors[index]['phone'],
+        relation: _visitors[index]['relation'],
+        date: _visitors[index]['date'],
+        timeline: "Last Visit",
+        buttonText1: "Remove",
+        buttonText2: "Schedule Visit",
+        buttonFunc1: () {},
+        buttonFunc2: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text('Are you sure you want to remove visitor ?'),
+                  content: Text(
+                      'Visitor will no longer be in your scheduled liste.'),
+                  actions: <Widget>[
+                    FlatButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Cancel')),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text('Remove'),
+                    )
+                  ],
+                );
+              });
         },
-      );
+        avatarLink: _visitors[index]['avatarLink'],
+      ),
+    );
+  },
+);
 
-Widget historyTab = ListView(children: <Widget>[
-  VisitorTile(avatarLink: 'assets/images/gateman/Ellipse.png', name: 'James Cameron', relation: 'Friend', timeline: 'Expected Arrival', phone: '65165151651', date: '2nd Nov 2019, Afternoon', buttonText1: 'Edit', buttonText2: 'Remove', buttonFunc1: (){}, buttonFunc2: (){},),
-  VisitorTile(avatarLink: 'assets/images/gateman/Ellipse.png', name: 'James Cameron', relation: 'Friend', timeline: 'Expected Arrival', phone: '65165151651', date: '2nd Nov 2019, Afternoon', buttonText1: 'Edit', buttonText2: 'Remove', buttonFunc1: (){}, buttonFunc2: (){},),
-  VisitorTile(avatarLink: 'assets/images/gateman/Ellipse.png', name: 'James Cameron', relation: 'Friend', timeline: 'Expected Arrival', phone: '65165151651', date: '2nd Nov 2019, Afternoon', buttonText1: 'Edit', buttonText2: 'Remove', buttonFunc1: (){}, buttonFunc2: (){},),
-
-],);
+Widget historyTab = ListView(
+  children: <Widget>[
+    VisitorTile(
+      avatarLink: 'assets/images/gateman/Ellipse.png',
+      name: 'James Cameron',
+      relation: 'Friend',
+      timeline: 'Expected Arrival',
+      phone: '65165151651',
+      date: '2nd Nov 2019, Afternoon',
+      buttonText1: 'Edit',
+      buttonText2: 'Remove',
+      buttonFunc1: () {},
+      buttonFunc2: () {},
+    ),
+    VisitorTile(
+      avatarLink: 'assets/images/gateman/Ellipse.png',
+      name: 'James Cameron',
+      relation: 'Friend',
+      timeline: 'Expected Arrival',
+      phone: '65165151651',
+      date: '2nd Nov 2019, Afternoon',
+      buttonText1: 'Edit',
+      buttonText2: 'Remove',
+      buttonFunc1: () {},
+      buttonFunc2: () {},
+    ),
+    VisitorTile(
+      avatarLink: 'assets/images/gateman/Ellipse.png',
+      name: 'James Cameron',
+      relation: 'Friend',
+      timeline: 'Expected Arrival',
+      phone: '65165151651',
+      date: '2nd Nov 2019, Afternoon',
+      buttonText1: 'Edit',
+      buttonText2: 'Remove',
+      buttonFunc1: () {},
+      buttonFunc2: () {},
+    ),
+  ],
+);
